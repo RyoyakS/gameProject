36d1e7292b41b99b5972cf88ea1f4c25a276dd1a
RyoyakS <bill121789@gmail.com>
2022-11-07 17:13:34 +0800

    PlayerScript CAMERA Done

--- Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
+++ Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
@@ -13,17 +13,17 @@ namespace UnityStandardAssets.Cameras
         // 		Pivot
         // 			Camera
 
-        [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
+        [SerializeField] private float m_MoveSpeed = 360f;                      // How fast the rig will move to keep up with the target's position.
         [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
         [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
         [SerializeField] private float m_TiltMax = 75f;                       // The maximum value of the x axis rotation of the pivot.
         [SerializeField] private float m_TiltMin = 45f;                       // The minimum value of the x axis rotation of the pivot.
-        [SerializeField] private bool m_LockCursor = false;                   // Whether the cursor should be hidden and locked.
-        [SerializeField] private bool m_VerticalAutoReturn = false;           // set wether or not the vertical axis should auto return
+        [SerializeField] private bool m_LockCursor = true;                   // Whether the cursor should be hidden and locked.
+        [SerializeField] private bool m_VerticalAutoReturn = false;           // set whether or not the vertical axis should auto return
 
         private float m_LookAngle;                    // The rig's y axis rotation.
         private float m_TiltAngle;                    // The pivot's x axis rotation.
-        private const float k_LookDistance = 100f;    // How far in front of the pivot the character's look target is.
+        private const float k_LookDistance = 10f;    // How far in front of the pivot the character's look target is.
 		private Vector3 m_PivotEulers;
 		private Quaternion m_PivotTargetRot;
 		private Quaternion m_TransformTargetRot;
@@ -70,7 +70,7 @@ namespace UnityStandardAssets.Cameras
         private void HandleRotationMovement()
         {
 			if(Time.timeScale < float.Epsilon)
-			return;
+				return;
 
             // Read the user input
             var x = CrossPlatformInputManager.GetAxis("Mouse X");
